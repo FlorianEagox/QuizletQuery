87d7aeca84a5e0ee0d62955610428c2b92057129
seth <seth@sethpainter.com>
2020-03-30 07:57:28 -0400

    Added message if the user doesn't ask a question.

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 knowledge_base = {}
 
+@app.route('/ask', defaults={'question': ''})
 @app.route('/ask/<question>')
 def root(question):
+	if not question:
+		return "please ask a question"
 	quizlet.ask_question(question, knowledge_base)
 	found_answer = [key for key in knowledge_base if quizlet.minify(question) in key]
 	if found_answer:
