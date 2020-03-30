e624dd884fa270d0c7824db2eed90466ca71d754
seth <seth@sethpainter.com>
2020-03-30 05:45:48 -0400

    Split the program into two files, one specifically to find quizlet answers.

--- app.py
+++ app.py
@@ -1,39 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
-import json
+import quizlet
 
-
-cse_request_url = 'https://www.googleapis.com/customsearch/v1/'
-cse_cx = '009651248597238434110:rv56718xitr'
-cse_key = 'AIzaSyDO22ReOgKlXN-1xQ3Mg78fiKTi-AB7NEg'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
-
-knowledge_base = {}
-
-def minify(string): return ''.join(e for e in string if e.isalnum()).lower()
-
-
-def find_answer_from_quizlet(url):
-	page = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
-	for el in [el.select('*') for el in page.select('.SetPageTerm-content')]:
-	 	knowledge_base[minify(el[0].get_text())] = el[-1].get_text()
-
-
-def get_search_results(question):
-	querystring = {'cx': cse_cx, 'key': cse_key, 'q': question}
-	results = json.loads(requests.request('GET', cse_request_url, params=querystring).text)
-	return [item['link'] for item in results['items']]
-
-
-def ask_question(question):
-	found_answer = []
-	for url in get_search_results(question):
-		found_answer = [key for key in knowledge_base if minify(question) in key]
-		if not found_answer:
-			find_answer_from_quizlet(url)
-	if found_answer:
-		return knowledge_base[found_answer[0]]
-	return found_answer
 
 
 running = True
@@ -42,9 +8,9 @@ while running:
 	if question in ["exit", "no", "quit"]:
 		running = False
 	else:
-		ask_question(question)
-		found_answer = [key for key in knowledge_base if minify(question) in key]
+		quizlet.ask_question(question)
+		found_answer = [key for key in quizlet.knowledge_base if quizlet.minify(question) in key]
 		if found_answer:
-			print(knowledge_base[found_answer[0]])
+			print(quizlet.knowledge_base[found_answer[0]])
 		else:
-			print(knowledge_base)
+			print(quizlet.knowledge_base)
---
+++ quizlet.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+cse_request_url = 'https://www.googleapis.com/customsearch/v1/'
+cse_cx = '009651248597238434110:rv56718xitr'
+cse_key = 'AIzaSyDO22ReOgKlXN-1xQ3Mg78fiKTi-AB7NEg'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
+
+
+def minify(string): return ''.join(e for e in string if e.isalnum()).lower()
+knowledge_base = {}
+
+def find_answer_from_quizlet(url):
+	page = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
+	for el in [el.select('*') for el in page.select('.SetPageTerm-content')]:
+	 	knowledge_base[minify(el[0].get_text())] = el[-1].get_text()
+
+
+def get_search_results(question):
+	querystring = {'cx': cse_cx, 'key': cse_key, 'q': question}
+	results = json.loads(requests.request('GET', cse_request_url, params=querystring).text)
+	return [item['link'] for item in results['items']]
+
+
+def ask_question(question):
+	found_answer = []
+	for url in get_search_results(question):
+		found_answer = [key for key in knowledge_base if minify(question) in key]
+		if not found_answer:
+			find_answer_from_quizlet(url)
+	if found_answer:
+		return knowledge_base[found_answer[0]]
+	return found_answer
