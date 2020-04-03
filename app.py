baaff773cefd2baaee2ae4a2e822ab8d050ee559
seth <seth@sethpainter.com>
2020-04-03 05:28:23 -0400

    It now returns all answers and the questions they came from

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ from flask import Flask, jsonify
 from secrets import token_urlsafe
 
 app = Flask(__name__)
-
+app.url_map.strict_slashes = False
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 knowledge_bases = dict()
 
 
-@app.route('/ask', defaults={'question': '', 'kb_token': ''})
-@app.route('/ask/<question>/', defaults={'kb_token': ''})
+@app.route('/ask')
+@app.route('/ask/')
+@app.route('/ask/<question>/')
 @app.route('/ask/<question>/<kb_token>/')
-def root(question, kb_token):
+def ask(question=None, kb_token=None):
+    # 400 error handling
     if not question:  # if the user doesn't ask a question
         return "please ask a question", 400
     if not kb_token:  # if the user doesn't provide a knowledge base token, create a new kn and give them a token
@@ -19,17 +22,24 @@ def root(question, kb_token):
         knowledge_bases[kb_token] = {}
     if not kb_token in knowledge_bases:
         return "Could not find the knowledge base " + str(kb_token), 404
+
     # populate their knowledge base with answers
-    quizlet.ask_question(question, knowledge_bases[kb_token])
-    found_answer = [key for key in knowledge_bases[kb_token] if quizlet.minify(
-        question) in key]  # Get all the questions containing their question
+    found_answer = quizlet.ask_question(question, knowledge_bases[kb_token])
 
     result = None
+    status = 'not_found'
+
     if found_answer:
-        result = knowledge_bases[kb_token][found_answer[0]]
+        result = found_answer
+        status = 'found'
     else:
         result = knowledge_bases[kb_token]
-    return jsonify(kb_token=kb_token, result=result)
+    return jsonify(kb_token=kb_token, status=status, result=result)
+
+
+@app.route('/')
+def root():
+    return 'Welcome to quizlet query API!\n Try the /ask/your+question+here endpoint!'
 
 
 @app.errorhandler(404)
@@ -39,5 +49,4 @@ def not_found(e):
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(port=8005)
