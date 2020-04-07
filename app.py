61773bca829a62281b81132f539e5581a8c21f7f
seth <seth@sethpainter.com>
2020-04-07 03:55:15 -0400

    Added expiration to a kb as well as route to get full kbs

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import quizlet
-from flask import Flask, jsonify
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from secrets import token_urlsafe
+
+from datetime import datetime, timedelta
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['CORS_HEADERS'] = 'Content-Type'
+# We need this cause 400/500 errors don't send cors =(
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+sched = BackgroundScheduler(daemon=True)
+sched.start()
 
 knowledge_bases = dict()
+
+
+def purge_token(token):
+	print('clearing job ' + token)
+	knowledge_bases.pop(token)
+	if sched.get_job(token):
+		sched.remove_job(token)
 
 
 @app.route('/ask')
@@ -14,39 +32,68 @@ knowledge_bases = dict()
 @app.route('/ask/<question>/')
 @app.route('/ask/<question>/<kb_token>/')
 def ask(question=None, kb_token=None):
-    # 400 error handling
-    if not question:  # if the user doesn't ask a question
-        return "please ask a question", 400
-    if not kb_token:  # if the user doesn't provide a knowledge base token, create a new kn and give them a token
-        kb_token = token_urlsafe(24)
-        knowledge_bases[kb_token] = {}
-    if not kb_token in knowledge_bases:
-        return "Could not find the knowledge base " + str(kb_token), 404
+	expiration = 30  # num minutes until this kb expires
 
-    # populate their knowledge base with answers
-    found_answer = quizlet.ask_question(question, knowledge_bases[kb_token])
+	# 400 error handling
+	if not question:  # if the user doesn't ask a question
+		return "please ask a question", 400
 
-    result = None
-    status = 'not_found'
+	if not kb_token:  # if the user doesn't provide a knowledge base token, create a new one and give it to them
+		kb_token = token_urlsafe(24)
+		knowledge_bases[kb_token] = {}
+	else:
+		# They're asking, so reset the expiration, cause we want to keep their kb
+		if sched.get_job(kb_token):
+			sched.remove_job(kb_token)
+	if not kb_token in knowledge_bases:
+		return "Could not find the knowledge base " + str(kb_token), 404
 
-    if found_answer:
-        result = found_answer
-        status = 'found'
-    else:
-        result = knowledge_bases[kb_token]
-    return jsonify(kb_token=kb_token, status=status, result=result)
+	user_expiration = request.args.get('expiration')
+	if user_expiration:
+		if user_expiration < 30 and user_expiration > 0:
+			expiration = user_expiration
+		else:
+			return 'invalid expiration. There is a maximum of 30 minutes', 400
+
+	# populate their knowledge base with answers
+	found_answer = quizlet.ask_question(question, knowledge_bases[kb_token])
+
+	result = None
+	status = 'not_found'
+
+	if found_answer:
+		result = found_answer
+		status = 'found'
+	else:
+		result = knowledge_bases[kb_token]
+	# schedule the kb to be deleted
+	sched.add_job(lambda: purge_token(kb_token), 'date',
+				  run_date=datetime.now() + timedelta(minutes=expiration), id=kb_token)
+	return jsonify(kb_token=kb_token, status=status, result=result, expiration=sched.get_job(kb_token).next_run_time)
+
+
+@app.route('/kb')
+@app.route('/kb/')
+@app.route('/kb/<token>/')
+def full_kb(token=None):
+	if token == None:
+		return 'You must provide a token', 400
+	if token in knowledge_bases:
+		return jsonify(knowledge_bases[token])
+	else:
+		return 'token not found, perhaps it expired', 400
 
 
 @app.route('/')
 def root():
-    return 'Welcome to quizlet query API!\n Try the /ask/your+question+here endpoint!'
+	return 'Welcome to quizlet query API!\n Try the /ask/your+question+here endpoint!'
 
 
 @app.errorhandler(404)
 def not_found(e):
-    print(e)
-    return "oof"
+	print(e)
+	return "oof"
 
 
 if __name__ == '__main__':
-    app.run(port=8005)
+	app.run(port=8005)
--- quizlet.py
+++ quizlet.py
@@ -7,34 +7,33 @@ cse_request_url = 'https://www.googleapis.com/customsearch/v1/'
 cse_cx = '009651248597238434110:rv56718xitr'
 cse_key = 'AIzaSyDO22ReOgKlXN-1xQ3Mg78fiKTi-AB7NEg'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
 
 
 def minify(string): return ''.join(e for e in string if e.isalnum()).lower()
 
 
 def find_answer_from_quizlet(url, knowledge_base):
-    page = BeautifulSoup(requests.get(
-        url, headers=headers).content, 'html.parser')
-    for el in [el.select('*') for el in page.select('.SetPageTerm-content')]:
-        knowledge_base[el[0].get_text()] = el[-1].get_text()
+	page = BeautifulSoup(requests.get(
+		url, headers=headers).content, 'html.parser')
+	for el in [el.select('*') for el in page.select('.SetPageTerm-content')]:
+		knowledge_base[el[0].get_text()] = el[-1].get_text()
 
 
 def get_search_results(question):
-    querystring = {'cx': cse_cx, 'key': cse_key, 'q': question}
-    results = json.loads(requests.request(
-        'GET', cse_request_url, params=querystring).text)
-    return [item['link'] for item in results['items']]
+	querystring = {'cx': cse_cx, 'key': cse_key, 'q': question}
+	results = json.loads(requests.request(
+		'GET', cse_request_url, params=querystring).text)
+	return [item['link'] for item in results['items']]
 
 
 def ask_question(question, knowledge_base={}):
-    found_answers = []
-    for url in get_search_results(question):
-        found_answers = [key for key in knowledge_base if minify(
-            question) in minify(key)]
-        if not found_answers:
-            find_answer_from_quizlet(url, knowledge_base)
-    print(found_answers)
-    if found_answers:
-        return [{'question': found_question, 'answer': knowledge_base[found_question]} for found_question in found_answers]
-    return found_answers
+	found_answers = []
+	for url in get_search_results(question):
+		found_answers = [key for key in knowledge_base if minify(
+			question) in minify(key)]
+		if not found_answers:
+			find_answer_from_quizlet(url, knowledge_base)
+	if found_answers:
+		return [{'question': found_question, 'answer': knowledge_base[found_question]} for found_question in found_answers]
+	return found_answers
